2b7990d60db1a4137b5df20c11602df8e7ef77ad
tanapat128 <tanapat.pm@mail.kmutt.ac.th>
2018-08-25 18:55:54 +0700

    edit code in getUser -try-catch, add s behind user var

--- src/main/java/th/ac/kmutt/sit/MyFirstService/controller/UserController.java
+++ src/main/java/th/ac/kmutt/sit/MyFirstService/controller/UserController.java
@@ -1,6 +1,7 @@
 package th.ac.kmutt.sit.MyFirstService.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -8,29 +9,14 @@ public class UserController {
     User allUser[] = { new User(1, "Adam"), new User(2, "Bobby"), new User(3, "Cara"), new User(4, "Danny"),
             new User(5, "Emily") };
 
-    @GetMapping("/user/1")
-    public User getUser1() {
-        return allUser[0];
-    }
-
-    @GetMapping("/user/2")
-    public User getUser2() {
-        return allUser[1];
-    }
-
-    @GetMapping("/user/3")
-    public User getUser3() {
-        return allUser[2];
-    }
-
-    @GetMapping("/user/4")
-    public User getUser4() {
-        return allUser[3];
-    }
-
-    @GetMapping("/user/5")
-    public User getUser5() {
-        return allUser[4];
+    @GetMapping("/users/{userID}")
+    public User getUser(@PathVariable String userID) {
+        int id = Integer.parseInt(userID) - 1;
+        try {
+            return allUser[id];
+        } catch (Exception ex) {
+            return null;
+        }
     }
 
     @GetMapping("/users")
